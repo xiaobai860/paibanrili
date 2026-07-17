5ecb7a0e862bc711b018055833a6222ffb2084bb
xx <byyx191@qq.com>
2026-07-18 04:22:08 +0800

    fix(widget): 修复日程小部件按钮高度设置
    - 将按钮高度从fillMaxHeight改为固定36dp
    - 更新应用版本号从2026071804到2026071805

--- app/src/main/java/com/schedulecalendar/app/widget/ScheduleGlanceWidget.kt
+++ app/src/main/java/com/schedulecalendar/app/widget/ScheduleGlanceWidget.kt
@@ -336,7 +336,7 @@ private fun ClockInWidgetContent() {
         Box(
             modifier = GlanceModifier
                 .width(40.dp)
-                .fillMaxHeight()
+                .height(36.dp)
                 .background(btnBgColor)
                 .cornerRadius(10.dp)
                 .clickable(actionRunCallback<ClockInAction>()),
