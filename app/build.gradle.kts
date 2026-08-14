3393e31186b9b6f797ac52eef737ac31500eec46
xx <byyx191@qq.com>
2026-08-14 08:29:29 +0800

    style(app): 更新排班应用图标及版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.schedulecalendar.app"
         minSdk        = 34
         targetSdk     = 36
-        versionCode   = 2026080301
-        versionName   = "2026080301"
+        versionCode   = 2026081401
+        versionName   = "2026081401"
     }
 
     signingConfigs {
